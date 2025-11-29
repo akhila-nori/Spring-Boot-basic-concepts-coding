79c40f97c06915e01b9df93f3d89fa91dc482458
akhila-nori <akhilanori2001@gmail.com>
2025-11-29 22:05:58 +0530

    Bean creation and dependency injection through Autowired with running project setup

---
+++ src/main/java/com/codingshuttle/akhila/module1Introduction/PaymentService.java
@@ -0,0 +1,10 @@
+package com.codingshuttle.akhila.module1Introduction;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+    public void pay() {
+        System.out.println("Playing ......");
+    }
+}
